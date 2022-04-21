78958186d9718e7611e2b46ea180ad82f2a0fcbf
Tiffany <tiffanyv1998@gmail.com>
2022-04-21 14:27:34 -0700

    wip addNewProduct page

--- src/components/AddNewProduct.js
+++ src/components/AddNewProduct.js
@@ -1,3 +1,4 @@
+import { handle } from "express/lib/application";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../custom-hooks";
@@ -42,4 +43,67 @@ export default function ProductForm() {
   };
 
   return <div>hello, i'm adding a new product</div>;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Plant title: </label>
+        <input
+          type="text"
+          name="title"
+          value={form.title}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Price: </label>
+        <input
+          type="number"
+          name="price"
+          value={form.price}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Description: </label>
+        <textarea
+          style={{ borderRadius: "5px" }}
+          name="description"
+          value={form.description}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Active: </label>
+        <input
+          type="checkbox"
+          name="isActive"
+          value={form.isActive}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Stock Quantity: </label>
+        <input
+          type="number"
+          name="inStockQuantity"
+          value={form.inStockQuantity}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Photo Link: </label>
+        <input
+          type="url"
+          name="photoLinkHref"
+          value={form.photoLinkHref}
+          onChange={handleChange}
+        />
+      </div>
+      <input type="submit" value="Add product" />
+      <button name="clear" onClick={() => history.push(`/products`)}>
+        Cancel
+      </button>
+    </form>
+  );
 }
