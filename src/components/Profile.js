ddd70710bc411873f1ee012c2e9a17a4104166e8
Tiffany <tiffanyv1998@gmail.com>
2022-04-22 11:01:43 -0700

    halt on profile, added editProduct

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useEffect, useState, useReducer } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../custom-hooks";
 
 //The profile will hold basic user information on the left. On the right, it'll have a list of orders the user has made. Each order will show the products "inside" them, the total price paid, the total quantity of items and when the order was placed.
 
 export default function Profile() {
+  const [profile, setProfile] = useState({});
+  const [orders, setOrders] = useState({});
+  const { token } = useAuth();
+
+  console.log({ token });
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const response = await fetch(
+          `http://localhost:4000/api/users/profile`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = await response.json();
+
+        setProfile(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchProfile();
+  }, [token]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const response = await fetch(`http://localhost:4000/api/orders`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await response.json();
+        setOrders(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchOrders();
+  }, [token]);
+
   //   return <div>Welcome to your profile, {user.email}</div>;
   return <div>Welcome to your profile!</div>;
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,7 @@
-//export { default as App } from "./App";
 export { default as Cart } from "./Cart";
 export { default as AllProductViews } from "./AllProductViews";
 export { default as LoginOrRegister } from "./LoginOrRegister";
 export { default as SingleProductView } from "./SingleProductView";
-
-// export { default as Cart } from "./Cart";
-// export { default as AllProductView } from "./AllProductViews";
-export { default as LoginOrRegister } from "./LoginOrRegister";
-// export { default as SingleProductView } from "./SingleProductView";
 export { default as Title } from "./Title";
 export { default as Nav } from "./Nav";
 export { default as Footer } from "./Footer";
