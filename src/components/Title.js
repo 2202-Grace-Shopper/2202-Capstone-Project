b4a21e2cd57fe390ea5c8bd1e184972fa3b1f56d
Kevin O'Brien <kdobrien22@gmail.com>
2022-04-25 15:20:37 -0700

    moved assets folder.  add: rendering the logo image with some styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,13 @@
 import React from "react";
+import logo from "../assets/PlantSiteLogo.JPG";
 
 export default function Title() {
   return (
     <>
       {/* <div>we'll put our logo here</div> */}
-      <img
-        src="/CapstoneCommerceSite/assets/PlantSiteLogo.jpg"
-        alt="logo pic"
-        className="logoImage"
-      ></img>
+      <div className="logoDiv">
+        <img src={logo} alt="logo pic" className="logoImage"></img>
+      </div>
     </>
   );
 }
