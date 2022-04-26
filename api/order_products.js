f118c1fe234d7a090e6a1e06e80adf1ec5fe61db
Kevin O'Brien <kdobrien22@gmail.com>
2022-04-26 11:14:58 -0700

    threw a live grenade and fragged carts and orders

--- api/order_products.js
+++ api/order_products.js
@@ -2,7 +2,8 @@ const express = require("express");
 const {
   destroyOrderProduct,
   updateOrderProduct,
-} = require("../db/models/order_products");
+  addProductToOrder,
+} = require("../db/models/cart");
 const authorizeUser = require("./utils");
 const orderProductsRouter = express.Router();
 
@@ -24,6 +25,21 @@ orderProductsRouter.patch(
     }
   }
 );
+orderProductsRouter.post("/", async (req, res, next) => {
+  console.log("Hi from router");
+  try {
+    const { productId, eachPrice, eachQuantity } = req.body;
+    const orderProduct = await addProductToOrder({
+      productId,
+      eachPrice,
+      eachQuantity,
+    });
+    console.log(orderProduct);
+    res.send(orderProduct);
+  } catch (err) {
+    next(err);
+  }
+});
 
 orderProductsRouter.delete(
   "/:orderProductId",
