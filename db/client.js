0d66481498cdba2f34064cdbfb3b7404b07a8090
Kevin O'Brien <99507332+KevinOBrien8@users.noreply.github.com>
2022-12-07 17:28:02 -0800

    client changes

--- db/client.js
+++ db/client.js
@@ -9,21 +9,21 @@ const DB_URL = DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`; /////////
 
 let client;
 
-client = new Client(DATABASE_URL);
+// client = new Client(DATABASE_URL);
 // github actions client config
-// if (process.env.CI) {
-//   client = new Client({
-//     host: "localhost",
-//     port: 5432, /////////////
-//     user: "postgres",
-//     password: "postgres",
-//     database: "postgres",
-//     ssl: { rejectUnauthorized: false },
-//   });
-// } else {
-//   // local / heroku client config
-//   client = new Client(DB_URL);
-// }
+if (process.env.CI) {
+  client = new Client({
+    host: "localhost",
+    port: 5432, /////////////
+    user: "postgres",
+    password: "postgres",
+    database: "postgres",
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  // local / heroku client config
+  client = new Client(DB_URL);
+}
 
 module.exports = client;
 
